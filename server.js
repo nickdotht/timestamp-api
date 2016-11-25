a2db2d001a21079c44a30d2dbacb2c438c4d3195
Nick Rameau <r4meau@gmail.com>
2016-11-25 15:48:59 -0800

    Hotfix

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.get('/:time', function(req, res, next) {
     var response = {};
     var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     var t = req.params.time;
-    
+
     if (!isNaN(t))
         var date = new Date(t * 1000);
     var time = (isNaN(t)) ? t : months[date.getMonth()] + ' ' + String(date.getDate()) + ', ' + String(date.getFullYear());
-    
+
     if (new Date(time) == "Invalid Date") {
         response.unix = null;
         response.natural = null;
@@ -29,6 +29,6 @@ app.get('/:time', function(req, res, next) {
     res.json(response);
 });
 
-app.listen(8080, function() {
-    console.log("Timestamp App listening on port 8080");
+app.listen(process.env.PORT || 8080, function() {
+    console.log("Timestamp App listening on port "+ (process.env.PORT || 8080));
 });
